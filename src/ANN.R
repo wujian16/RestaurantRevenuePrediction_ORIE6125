cbd5e3057075b7cda26d6dc5938c9e192a61c495
Jian Wu <wujian046@gmail.com>
2015-05-19 23:03:15 -0500

    ann

---
+++ src/ANN.R
@@ -0,0 +1,12 @@
+data1<-read.csv("train.csv")
+data2<-read.csv("train_DR.csv",header=FALSE)
+data = as.data.frame(cbind(data1,data2))
+data$Open.Date  = as.Date(data$Open.Date, "%m/%d/%Y")
+data$City.Group = as.factor(data$City.Group)
+data$Type=as.factor(data$Type)
+data$revenue=log(data$revenue)
+
+library(nnet)
+M=max(data$revenue)
+data.net = nnet(revenue/M~Open.Date+City.Group+Type+V1+V2+V3, data = data, size=20)
+plot(data.net$fit*M)
